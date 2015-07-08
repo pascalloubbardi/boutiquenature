e5f63ef5d59e57202ded7b4e00f7627c0a79c43b
Pascal LOUBBARDI <pascal.loubbardi@gmail.com>
2015-07-08 02:01:51 +0200

    quelques modif

--- app/overrides/logo.rb
+++ app/overrides/logo.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_header", 
                      :name => "logo", 
                      :replace_contents => "#logo", 
-                     :text => "<span class='glyphicon glyphicon-leaf' aria-hidden='true'></span>&nbsp;<%= link_to 'La Boutique Nature', products_path %>")
+                     :text => "<span class='glyphicon glyphicon-leaf' aria-hidden='true'></span>&nbsp;<%= link_to 'La Boutique Nature', root_path %>")
